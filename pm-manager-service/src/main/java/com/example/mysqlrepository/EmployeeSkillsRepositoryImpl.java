63e80c334b8fa06b1fd946fcfeb62dae6f5fa178
Hemanshu Dharmik <hemanshu.edu@gmail.com>
2023-08-11 11:38:33 +0530

    .

--- pm-manager-service/src/main/java/com/example/mysqlrepository/EmployeeSkillsRepositoryImpl.java
+++ pm-manager-service/src/main/java/com/example/mysqlrepository/EmployeeSkillsRepositoryImpl.java
@@ -5,6 +5,7 @@ import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
+import javax.persistence.TypedQuery;
 
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
@@ -24,8 +25,9 @@ public class EmployeeSkillsRepositoryImpl  implements EmployeeSkillsRepositoryCu
 	@Override
 	public List<Employee> getEmployeesBySkill(String skillName) {
 		// TODO Auto-generated method stub
-		 Query query = entityManager.createNativeQuery("select * from employee e inner join employee_skills es where e.employee_id = es.employee_id \r\n" + 
-		 		"and e.user_type = 'MEMBER' and e.availability=1 and es.skill_id= (Select skill_id from skills where skill_name=:skillName); ", 
+		 Query query = entityManager.createNativeQuery(
+				 "select * from employee e inner join employee_skills es where e.employee_id = es.employee_id \r\n" 
+		 + "and e.user_type = 'EMPLOYEE' and e.availability=1 and es.skill_id= (Select skill_id from skills where skill_name=:skillName); ", 
 		 		Employee.class);
 		 query.setParameter("skillName", skillName);
 		 
@@ -33,11 +35,13 @@ public class EmployeeSkillsRepositoryImpl  implements EmployeeSkillsRepositoryCu
 	}
 	
 	
-	@SuppressWarnings("unchecked")
 	@Override
 	public List<EmployeeSkills> getEmployeeSkills(Long employeeId) {
 		// TODO Auto-generated method stub
-		Query query = entityManager.createQuery("from EmployeeSkills as es join Employee as e on e.employeeId = es.employeeId where e.employeeId=:theId");
+		TypedQuery<EmployeeSkills> query = entityManager.createQuery(
+	            "SELECT es FROM EmployeeSkills es JOIN es.employeeId e WHERE e.employeeId = :employeeId",
+	            EmployeeSkills.class);
+
 
 			 query.setParameter("employeeId", employeeId);
 			 
